bd13c11e54da662c0072c26d9aa6d2f9d8107037
Erik Bard <erik.bard@outlook.com>
2021-04-22 16:44:32 +0200

    Added MS Unit Test project to Solution. Created order and folders. Created few simple tests. More advanced tests are required.

---
+++ HotelIsaac_Test/Models_Test/BookingTest.cs
@@ -0,0 +1,41 @@
+﻿using HotelIsaac.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelIsaac_Test.Models_Test
+{
+    [TestClass]
+    public class BookingTest
+    {
+        [TestMethod]
+        public void BookingModelTest()
+        {
+            Booking booking = new Booking();
+            List<Review> reviews = new List<Review>();
+            List<Bookingsroom> bookedRoom = new List<Bookingsroom>();
+            Customer customer = new Customer();
+            staff testStaff = new staff();
+            testStaff.Firstname = "Kalle";
+
+            booking.Id = 1;
+            booking.Qtypersons = 2;
+            booking.Specialneeds = "Quiet";
+            booking.Customersid = 1;
+            booking.Startdate = DateTime.Now;
+            booking.Enddate = DateTime.Now;
+            booking.Eta = DateTime.Now;
+            booking.Extrabed = true;
+            booking.Reviews = reviews;
+            booking.Bookingsrooms = bookedRoom;
+            booking.Staffid = 1;
+            booking.Staff = testStaff;
+            booking.Timearrival = DateTime.Now;
+            booking.Timedeparture = DateTime.Now;
+            booking.Customers = customer;
+        }
+    }
+}
